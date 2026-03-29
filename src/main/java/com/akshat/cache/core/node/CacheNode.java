e4e6141503c789d809f51a37508e1e9bfcd7dd62
mishra-ji20 <mishra.akshat1020@gmail.com>
2026-03-29 14:05:40 +0530

    Addition of creation time in Node

--- src/main/java/com/akshat/cache/core/node/CacheNode.java
+++ src/main/java/com/akshat/cache/core/node/CacheNode.java
@@ -1,15 +1,18 @@
 package com.akshat.cache.core.node;
 
+import java.time.Instant;
+
 public class CacheNode<K , V> {
     K key ;
     V value ;
     CacheNode <K  , V> next;
     CacheNode <K , V> prev;
-
+    long creationTime;
     public CacheNode(K key , V value , CacheNode<K , V> next , CacheNode <K , V> prev){
         this.key =  key;
         this.value = value;
         this.next = next;
         this.prev = prev;
+        creationTime = Instant.now().toEpochMilli();
     }
 }

92550fe58a4fe6b2b501ae168e2ea4c0f088a068
mishra-ji20 <mishra.akshat1020@gmail.com>
2026-03-31 01:43:37 +0530

    improvemnet of eviction policy function

--- src/main/java/com/akshat/cache/core/eviction/EvictionPolicy.java
+++ src/main/java/com/akshat/cache/core/eviction/EvictionPolicy.java
@@ -3,6 +3,8 @@ package com.akshat.cache.core.eviction;
 import com.akshat.cache.core.node.CacheNode;
 
 public interface EvictionPolicy <K , V> {
-    void keysAccessed(CacheNode<K , V> node);
-    void evict();
+    void onAccess(CacheNode<K , V> node);
+    void onInsert(CacheNode <K , V> node);
+    void onDelete(CacheNode <K , V> node);
+    K evict();
 }
--- src/main/java/com/akshat/cache/core/eviction/LRUEvictionPolicy.java
+++ src/main/java/com/akshat/cache/core/eviction/LRUEvictionPolicy.java
@@ -8,25 +8,45 @@ import com.akshat.cache.core.node.CacheNode;
 public class LRUEvictionPolicy <K , V> implements EvictionPolicy <K , V>  {
     CacheNode<K , V> dummyHead;
     CacheNode<K , V> dummyTail;
-    int currSize;
     public LRUEvictionPolicy(){
         dummyHead = new CacheNode<>(null , null , null , null);
         dummyTail = new CacheNode<>(null , null , null  , null);
         dummyHead.setNext(dummyTail);
         dummyTail.setNext(dummyHead);
-        currSize = 0 ;
     }
 
 
     //checks to add if node exists or not should be made in cache Application
     @Override
-    public void keysAccessed(CacheNode<K, V> node) {
-
+    public void onAccess(CacheNode<K, V> node) {
+        onDelete(node);
+        onInsert(node);
 
     }
 
     @Override
-    public void evict() {
+    public void onInsert(CacheNode<K, V> node) {
+        node.setPrev(dummyHead);
+        node.setNext(dummyHead.getNext());
+        dummyHead.getNext().setPrev(node);
+        dummyHead.setNext(node);
+
+    }
+
+    @Override
+    public void onDelete(CacheNode<K, V> node) {
+        node.getPrev().setNext(node.getNext());
+        node.getNext().setPrev(node.getPrev());
+    }
+
+    @Override
+    public K evict() {
+        K key = null ;
+        if(dummyTail.getPrev() != dummyHead){
+            key= dummyTail.getPrev().getKey();
+            onDelete(dummyTail.getPrev());
+        }
+        return key;
 
     }
 }
